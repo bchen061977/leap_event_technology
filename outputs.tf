6a64d6de24853a8145412922555fdb2d1c061141
EC2 Default User <ec2-user@ip-10-1-1-90.us-west-2.compute.internal>
2026-02-03 13:54:45 +0000

    outputs

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "my_name" {
+  description = "My Name Hello from brian"
+  value = "Brian Chen"
+  type = String
+}
+
+output "position" {
+  description = "I'm a expert problem solver"
+  value = "Senior DevOps Engineer"
+  type = String
+}
